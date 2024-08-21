ad20186472841777f0a693f9e1c935b2c04c1c7f
folarin <folarinoyenuga@folarins-MacBook-Air.local>
2024-08-21 11:57:57 +0100

    refactor the program and tests to use switches

--- chapter-1/hello.go
+++ chapter-1/hello.go
@@ -4,13 +4,45 @@ import (
 	"fmt"
 )
 
+const yoruba = "Yoruba"
+const spanish = "Spanish"
+const german = "German"
 const greeting = "Hello, "
+const yorubaGreeting = "Bawo ni, "
+const spanishGreeting = "Hola, "
+const germanGreeting = "Hallo, "
 // domain code: this means this function returns a string
-func Hello(name string) string {
-	return greeting + name
+func Hello(name, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	return greetingPrefix(language) + name
+	// if language == yoruba {
+	// 	return yorubaGreeting + name
+	// }
+	// if language == spanish {
+	// 	return spanishGreeting + name
+	// }
+	// if language == german {
+	// 	return germanGreeting + name
+	// }
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case yoruba:
+		prefix = yorubaGreeting
+	case spanish:
+		prefix = spanishGreeting
+	case german:
+		prefix = germanGreeting
+	default:
+		prefix = greeting
+	}
+	return
 }
 
 // entry point of the program: exposes the domain code to the outside world
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "German"))
 }
