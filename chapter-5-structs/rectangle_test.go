90e9e481970685b9c92ee133e3c6cdb97b614f6b
folarin <folarinoyenuga@folarins-MacBook-Air.local>
2024-08-31 12:13:17 +0100

    add methods to an interface, create types with structs and testing using interface methods

--- chapter-5-structs/rectangle_test.go
+++ chapter-5-structs/rectangle_test.go
@@ -3,26 +3,43 @@ package rectangle
 import "testing"
 
 func TestArea(t *testing.T) {
-	t.Run("test area of a rectangle", func(t *testing.T) {
-		rectangle := Rectangle{7.7, 12.5}
-		got := rectangle.Area()
-		want := 96.25
-	
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{7.7, 12.5}, 96.25},
+		{Circle{10}, 314.1592653589793},
+	}
 
-	t.Run("test the area of a circle", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	// iterate over the test cases
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
 		}
-	})
+	}
 }
+// func TestArea(t *testing.T) {
+// 	t.Run("test area of a rectangle", func(t *testing.T) {
+// 		rectangle := Rectangle{7.7, 12.5}
+// 		got := rectangle.Area()
+// 		want := 96.25
+	
+// 		if got != want {
+// 			t.Errorf("got %.2f want %.2f", got, want)
+// 		}
+// 	})
+
+// 	t.Run("test the area of a circle", func(t *testing.T) {
+// 		circle := Circle{10}
+// 		got := circle.Area()
+// 		want := 314.1592653589793
+
+// 		if got != want {
+// 			t.Errorf("got %.2f want %.2f", got, want)
+// 		}
+// 	})
+// }
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.7, 5.8}
