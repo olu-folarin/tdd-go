6721de364aa0865c3ab15c9aada4871cb5ded43a
folarin <folarinoyenuga@folarins-MacBook-Air.local>
2024-09-01 19:43:31 +0100

    add a withdraw function and test it
    in relation to TDD pointers and errors

--- chapter-6-pointers-errors/wallet.go
+++ chapter-6-pointers-errors/wallet.go
@@ -8,6 +8,9 @@ type Wallet struct {
 	balance Bitcoin
 }
 
+type Stringer interface {
+	String() string
+}
 
 func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Printf("address of balance in Deposit is %p and balance is %d \n", &w.balance, w.balance)
@@ -17,4 +20,14 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 
 func (w *Wallet) Total() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	fmt.Printf("address of balance in Withdraw is %p and balance before withdrawal is %d \n", &w.balance, w.balance)
+	w.balance -= amount
+	fmt.Printf("address of balance in Withdraw is %p and new balance is %d \n", &w.balance, w.balance)
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
--- chapter-6-pointers-errors/wallet_test.go
+++ chapter-6-pointers-errors/wallet_test.go
@@ -1,18 +1,31 @@
 package wallet
 
-import ("testing"
-		"fmt"
-	)
+import (
+	"fmt"
+	"testing"
+)
 
-	// run type conversion from int to Bitcoin
+// run type conversion from int to Bitcoin
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(77))
-	got := wallet.Total()
-	fmt.Printf("address od balance in test is %p. it contains %d btc \n", &wallet.balance, wallet.balance)
-	want := Bitcoin(77)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet.Deposit(Bitcoin(77))
+		got := wallet.Total()
+		fmt.Printf("address od balance in test is %p. it contains %d btc \n", &wallet.balance, wallet.balance)
+		want := Bitcoin(77)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet.Withdraw(Bitcoin(9))
+		got := wallet.Total()
+		want := Bitcoin(68)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
